24ba04f365f4895b13e89f04cc63bab7325d80dc
MomoCow <momocow.me@gmail.com>
2019-12-23 02:24:54 +0800

    :bookmark: v0.1.0

---
+++ dist/ogame-discoverer-kit.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        OGDK: OGame Discoverer Kit 🚀
+// @version     0.1.0
+// @author      MomoCow
+// @description Everything a discoverer must have at hand
+// @namespace   code.cow.moe
+// @include     https://*.ogame.gameforge.com/game/index.php?*
+// @run-at      document-body
+// @require     https://cdn.jsdelivr.net/npm/vue@2.6.11
+// @require     https://unpkg.com/vue-i18n@8.15.3/dist/vue-i18n.min.js
+// @require     https://unpkg.com/vuex@3.1.2/dist/vuex.min.js
+// @grant       unsafeWindow
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_deleteValue
+// @grant       GM_addValueChangeListener
+// ==/UserScript==
