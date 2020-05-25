5e8556d12593681716375a9e0958add43b621920
momocow <momocow.me@gmail.com>
2020-05-25 19:07:39 +0800

    :sparkles: Numeral supports math operations.

--- packages/ogame-sdk/utils.ts
+++ packages/ogame-sdk/utils.ts
@@ -6,6 +6,10 @@ export interface NumeralFormatOptions {
   delimiter: string
 }
 
+export interface NumberLike {
+  valueOf (): number
+}
+
 /**
  * OGame number formatted with fragmentation for readability.
  */
@@ -14,26 +18,6 @@ export class Numeral extends Number {
     fragment: 3,
     decimalPoint: '.',
     delimiter: ','
-  }
-
-  public format (options?: Partial<NumeralFormatOptions>): string {
-    const {
-      fragment, decimalPoint, delimiter
-    } = Object.assign({}, Numeral.formatOptions, options)
-    const [integer, fraction = ''] = super.toString().split('.')
-    let tokens: string[] = []
-    for (
-      let idx = 0, len = (integer.length % fragment) || fragment;
-      idx + len <= integer.length;
-      idx += len, len = fragment
-    ) {
-      tokens.push(integer.substr(idx, len))
-    }
-    return tokens.join(delimiter) + (fraction ? decimalPoint + fraction : '')
-  }
-  
-  public toString (): string {
-    return this.format()
   }
 
   public static parse (
@@ -48,5 +32,63 @@ export class Numeral extends Number {
       .replace(new RegExp(_.escapeRegExp(decimalPoint)), '.')
       .replace(new RegExp(_.escapeRegExp(delimiter), 'g'), '')
     )
+  }
+
+  // Math Utilities methods
+  // to stick math operators to return Numeral
+
+  public static add (value1: NumberLike, value2: NumberLike): Numeral {
+    return new Numeral(value1.valueOf() + value2.valueOf())
+  }
+
+  public static subtract (value1: NumberLike, value2: NumberLike): Numeral {
+    return new Numeral(value1.valueOf() - value2.valueOf())
+  }
+
+  public static multiply (value1: NumberLike, value2: NumberLike): Numeral {
+    return new Numeral(value1.valueOf() * value2.valueOf())
+  }
+
+  public static divide (value1: NumberLike, value2: NumberLike): Numeral {
+    return new Numeral(value1.valueOf() / value2.valueOf())
+  }
+
+  public format (options?: Partial<NumeralFormatOptions>): string {
+    const {
+      fragment, decimalPoint, delimiter
+    } = Object.assign({}, Numeral.formatOptions, options)
+    const [integer, fraction = ''] = super.toString().split('.')
+    const tokens: string[] = []
+    let idx: number = 0
+    let len: number = integer.length % fragment
+    if (len === 0) {
+      len = fragment
+    }
+    for (; idx + len <= integer.length; idx += len, len = fragment) {
+      tokens.push(integer.substr(idx, len))
+    }
+    const formattedInteger = tokens.join(delimiter)
+    const formattedFraction = fraction.length > 0 ? decimalPoint + fraction : ''
+    return formattedInteger + formattedFraction
+  }
+
+  public toString (): string {
+    return this.format()
+  }
+
+  public add (value: NumberLike): Numeral {
+    return Numeral.add(this, value)
+  }
+
+  public subtract (value: NumberLike): Numeral {
+    return Numeral.subtract(this, value)
+  }
+
+  public multiply (value: NumberLike): Numeral {
+    return Numeral.multiply(this, value)
+  }
+
+  public divide (value: NumberLike): Numeral {
+    return Numeral.divide(this, value)
   }
 }
