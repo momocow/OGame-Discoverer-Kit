3023ad65ce7abff6c5389ef9dd27a59c8d3802fb
momocow <momocow.me@gmail.com>
2020-06-03 18:27:29 +0800

    :construction: Work on message page.

--- packages/ogame-sdk/lib/pages/messages.ts
+++ packages/ogame-sdk/lib/pages/messages.ts
@@ -1,7 +1,31 @@
-// export interface Tab {
-//   fleets: ''
-//   communication: ''
-//   market: ''
-//   system: ''
-//   favorite: ''
+import { DomBranch } from '@/utils'
+
+export class Message implements DomBranch {
+  constructor (
+    public $: JQuery<HTMLLIElement>
+  ) { }
+
+  public getId (): string {
+    return this.$.data('msg-id')
+  }
+
+  public getContent (): string {
+
+  }
+}
+
+export interface Subtab extends DomBranch {
+  getMessages (): Message[]
+}
+
+// export const enum Tab {
+//   FleetSpy = 20,
+//   FleetBattle = 21,
+//   FleetExpedition = 22,
+//   FleetTransport = 23,
+//   FleetOther = 24
+// }
+
+// export function getMessages (tab: Tab): Array<Message> {
+
 // }
--- packages/ogame-sdk/lib/utils.ts
+++ packages/ogame-sdk/lib/utils.ts
@@ -1,3 +1,7 @@
+export interface DomBranch<T extends HTMLElement = HTMLElement> {
+  $: JQuery<T>
+}
+
 export function $parseInt (str: string, radix?: number): number {
   const n = parseInt(str, radix)
   if (Number.isNaN(n)) {
