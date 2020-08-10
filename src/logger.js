16e50c13b1fcf5f21ad9a184295fd3c2c5b1f4d0
momocow <momocow.me@gmail.com>
2020-08-10 11:11:56 +0800

    :boom: Hard reset.

--- src/logger.js
+++
@@ -1,4 +0,0 @@
-import { Logger } from './utils/logger'
-const logger = new Logger('OGDK')
-logger.setLevel(Logger.INFO)
-export default logger
--- src/utils/datetime.js
+++
@@ -1,7 +0,0 @@
-export function today (now = new Date()) {
-  return new Date(now.getFullYear(), now.getMonth(), now.getDate())
-}
-
-export function tomorrow (now = new Date()) {
-  return new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1)
-}
