49120267583281411a899642d6e423068c0c4eb5
momocow <momocow.me@gmail.com>
2020-08-12 15:56:21 +0800

    :package: Change update and download urls.

--- dist/ogame-discoverer-kit.user.js
+++ dist/ogame-discoverer-kit.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name        OGDK: OGame Discoverer Kit 🚀
-// @version     0.1.0
+// @version     0.1.1
 // @author      MomoCow
 // @description Everything a discoverer must have at hand
-// @namespace   code.cow.moe
+// @namespace   cow.moe
 // @include     https://*.ogame.gameforge.com/game/index.php?*
 // @run-at      document-body
+// @updateURL   https://github.com/momocow/OGame-Discoverer-Kit/releases/latest/download/ogdk-core.meta.js
+// @downloadURL https://github.com/momocow/OGame-Discoverer-Kit/releases/latest/download/ogdk-core.user.js
 // @require     https://cdn.jsdelivr.net/npm/vue@2.6.11
 // @require     https://unpkg.com/vue-i18n@8.15.3/dist/vue-i18n.min.js
 // @require     https://unpkg.com/vuex@3.1.2/dist/vuex.min.js
