a18265d39ca3fb16f16713295dc46511158fddac
momocow <momocow.me@gmail.com>
2020-08-13 20:22:39 +0800

    :construction: Work on migration confirm dialog.

--- userscripts/core/index.js
+++ userscripts/core/index.js
@@ -1,7 +1,21 @@
-// import React, { Component } from 'react'
-// import ReactDOM from 'react-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import { Local, VERSION } from '@lib/storage'
+import MigrationConfirm from './components/MigrationConfirm'
 
 (function () {
-  // ReactDOM.render(<Hello />, document.body)
-  // GM_registerMenuCommand()
+  const document = unsafeWindow.document
+  const rootEl = document.createElement('div')
+  rootEl.id = 'ogdk-root'
+  document.body.appendChild(rootEl)
+
+  if (Local.isStale()) {
+    ReactDOM.render(
+      <MigrationConfirm
+        legacyVersion={Local.getVersion()}
+        targetVersion={VERSION} />,
+      rootEl
+    )
+  }
 })()
