ee31001eb604ef55c2fd3faac47652f1ccb4974c
momocow <momocow.me@gmail.com>
2020-08-14 19:35:35 +0800

    :recycle: Split component, Confirm.

--- userscripts/core/components/MigrationConfirm.js
+++ userscripts/core/components/MigrationConfirm.js
@@ -7,7 +7,7 @@ import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
 
-import { goog } from '@lib/utils'
+import { goog } from '@lib/utils/misc'
 
 export default class MigrationConfirm extends React.Component {
   static propTypes = {
